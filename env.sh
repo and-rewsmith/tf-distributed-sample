8cc01a3cf793bb16be314f01bfd79c62e4565fec
Andrew <andrew.smith.recruiting@gmail.com>
2024-10-23 17:33:41 -0700

    prog

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 export TF_CONFIG='{
   "cluster": {
-    "worker": ["<IP or hostname of worker0>:12345", "<IP or hostname of worker1>:23456"]
+    "worker": ["192.168.50.17:12345", "192.168.50.130:23456"]
   },
   "task": {"type": "worker", "index": 0}
 }'
